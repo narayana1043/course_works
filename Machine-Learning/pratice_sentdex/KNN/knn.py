efe5d17af103b3258577f25caa408dc97b6aa4e3
Veera Marni <narayana1043@gmail.com>
2016-06-15 00:51:21 -0400

    ML-sentdex-KNN commit

---
+++ Machine-Learning/pratice_sentdex/KNN/knn.py
@@ -0,0 +1,48 @@
+import numpy as np
+from math import sqrt
+import matplotlib.pyplot as plt
+import warnings
+from matplotlib import style
+from collections import Counter
+style.use('fivethirtyeight')
+
+
+dataset = {'k':[[1,2],[2,3],[3,1]], 'r':[[6,5],[7,7],[8,6]]}
+new_features = [5,7]
+
+def k_nearest_neighbors(data, predict, k=3):
+    if len(data) >= k:
+        warnings.warn('K is set to a value less than total voting groups')
+
+    # knn Algo
+    distances = []
+    for group in data:
+        for features in data[group]:
+            # slow, works only in 2 dimensions, we can overcome that with single for looop
+            # euclidean_distance = sqrt( (features[0]-predict[0])**2 + (features[1]-predict[1])**2 )
+            # using numpy.sqrt and numpy.sum, also slow but faster than previous, no need addtional for loop
+            # euclidean_distance = np.sqrt(np.sum(np.array(features)-np.array(predict))**2)
+
+            # faster, linalg - linear algebra, refer numpy doc's
+            eculidean_distance = np.linalg.norm(np.array(features)-np.array(predict))
+            distances.append([eculidean_distance, group])
+
+    votes = [i[1] for i in sorted(distances)[:k]]
+    # print(votes)
+    # print(Counter(votes).most_common(1))
+    # print(Counter(votes).most_common(2))
+    vote_result = Counter(votes).most_common(1)[0][0]
+
+    return vote_result
+
+result = k_nearest_neighbors(dataset, new_features, k=5)
+print(result)
+
+# [[plt.scatter(ii[0], ii[1], s=100, color=i) for ii in dataset[i]] for i in dataset]
+# # for i in dataset:
+# #     for ii in dataset[i]:
+# #         plt.scatter(ii[0], ii[1], s=100, color=i)
+# plt.scatter(new_features[0], new_features[1], s=100, color='green')
+# plt.show()
+
+
---
+++ Machine-Learning/pratice_sentdex/KNN/knn_breast_cancer.py
@@ -0,0 +1,74 @@
+import numpy as np
+from math import sqrt
+import matplotlib.pyplot as plt
+import warnings
+import pandas as pd
+import random
+from collections import Counter
+
+def k_nearest_neighbors(data, predict, k=3):
+    if len(data) >= k:
+        warnings.warn('K is set to a value less than total voting groups')
+
+    # knn Algo
+    distances = []
+
+    for group in data:
+        for features in data[group]:
+            eculidean_distance = np.linalg.norm(np.array(features)-np.array(predict))
+            distances.append([eculidean_distance, group])
+
+    votes = [i[1] for i in sorted(distances)[:k]]
+    vote_result = Counter(votes).most_common(1)[0][0]
+    confidence = Counter(votes).most_common(1)[0][1] / k
+    # print(vote_result, confidence)
+
+    return vote_result, confidence
+
+accuracies = []
+
+
+# it will take longer time to run compared to sklearn is because it can be threaded heavily and sklearn uses threading with n_jobs parameter
+
+for i in range(25):
+    df = pd.read_csv('./uci_breast_cancer_dataset/breast-cancer-wisconsin.data.txt')
+    df.replace('?', -99999, inplace=True)
+    df.drop(['id'], 1, inplace=True)
+    # astype(float) to convert all the data to folat variables
+    # .values.tolist() is not to loose the relationship of the features when shuffled
+    full_data = df.astype(float).values.tolist()
+    # print(full_data[:5])
+    random.shuffle(full_data)
+    # print(20*'#')
+    # print(full_data[:5])
+
+    test_size = 0.3
+    train_set = {2:[], 4:[]}
+    test_set = {2:[], 4:[]}
+    train_data = full_data[:-int(test_size*len(full_data))]
+    test_data = full_data[-int(test_size*len(full_data)):]
+
+    for i in train_data:
+        train_set[i[-1]].append(i[:-1])
+    for i in test_data:
+        test_set[i[-1]].append(i[:-1])
+
+    correct = 0
+    total = 0
+
+    for group in test_set:
+        for data in test_set[group]:
+            vote, confidence = k_nearest_neighbors(train_set, data, k=5)
+            if group == vote:
+                correct+=1
+            else:
+                print(confidence)
+            total+=1
+
+    print('Accuracy:', correct/total)
+    accuracies.append(correct/total)
+
+print(sum(accuracies)/len(accuracies))
+
+
+
